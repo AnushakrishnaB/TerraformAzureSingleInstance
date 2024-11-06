f85064d126f955fcb08a1792632a2a5a2af38d67
Anusha Besta <Anusha.Besta@lvs1.com>
2024-11-06 06:43:13 -0500

    Terraform Azure VM Base Code-v1

--- variables.tf
+++ variables.tf
@@ -4,3 +4,25 @@ variable "prod_subnet-1_cidr" {}
 variable "subscription_id" {}
 variable "username" {}
 variable "vm_name" {}
+
+# variable "resource_group_name" {
+#   type        = "string"
+#   default = "dev_rg"
+# }
+# variable "countrylocation" {
+#   type        = "string"
+#   default = "canadacentral"
+# }
+
+# variable "azs" {
+#   description = "Run the VM in these Availability Zones"
+#   default = ["canada central", "canada east"]
+# }
+
+# variable "instance_type" {
+#   default = {
+#     dev = "t2.nano"
+#     test = "t2.micro"
+#     prod = "t2.medium"
+#     }
+# }
